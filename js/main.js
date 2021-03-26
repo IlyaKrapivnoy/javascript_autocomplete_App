fc8753dff4916fe2b23e8f2227e66362e5bc02be
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-26 23:09:16 +0200

    regex

--- js/main.js
+++ js/main.js
@@ -6,7 +6,12 @@ const searchStates = async searchText => {
     const res = await fetch('../data/states.json');
     const states = await res.json();
 
-    console.log(states)
+    // Get matches to current text input
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    })
+    console.log(matches)
 }
 
 search.addEventListener('input', () => searchStates(search.value))
